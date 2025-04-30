3bd181bf1a17d7b952fb2e2c636f59735ae04335
Elias P. <117754325+NightmoonFF@users.noreply.github.com>
2025-04-30 09:36:50 +0200

    update

--- Kollegeni/DTOs/BookingResponseDto.cs
+++ Kollegeni/DTOs/BookingResponseDto.cs
@@ -2,8 +2,8 @@
 {
     public class BookingResponseDto
     {
-        public DateTime StartTidspunkt { get; set; }
-        public DateTime SlutTidspunkt { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
         public int BookingId { get; set; }
     }
 }
