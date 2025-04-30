b2724cf3a08e24a076a1d7194647ef0fd7b82091
Elias P. <117754325+NightmoonFF@users.noreply.github.com>
2025-04-30 10:37:01 +0200

    what

---
+++ Kollegeni/Data/DatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Kollegeni.Models;
+
+namespace Kollegeni.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(): base("Kollegeni") { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Residency> Residency { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+
+
+}
