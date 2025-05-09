3b7f2f99b376c6e88016da7e14b86a6451183327
Elias P. <117754325+NightmoonFF@users.noreply.github.com>
2025-05-09 09:41:16 +0200

    gg

--- Kollegeni/Controllers/CalendarController.cs
+++ Kollegeni/Controllers/CalendarController.cs
@@ -221,19 +221,7 @@ namespace Kollegeni.Controllers
 
         public JsonResult GetBookingDetails(int id)
         {
-            var booking = _context.Bookings
-                .Where(b => b.Id == id)
-                .Select(b => new
-                {
-                    id = b.Id,
-                    start = b.StartTime.ToString("yyyy-MM-dd"),
-                    timeSlot = b.StartTime.ToString("HH:mm"), // Udled timeSlot fra StartTime
-                    end = b.EndTime.ToString("yyyy-MM-dd"),
-                    roomId = b.RoomId,
-                    roomName = b.Room.Name,
-                    residency = b.Residency.Address
-                })
-                .FirstOrDefault();
+            var booking = GetBookingDetailsSimple(id);
 
             if (booking == null)
             {
@@ -243,7 +231,12 @@ namespace Kollegeni.Controllers
             return Json(new { success = true, data = booking });
         }
 
-
+        public Booking GetBookingDetailsSimple(int id)
+        {
+            return _context.Bookings
+                .Where(b => b.Id == id)
+                .FirstOrDefault();
+        }
     }
 
 }
